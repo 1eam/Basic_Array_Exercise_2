2ed72d762dce260864be0bf32457e99cdc7a45d1
Esther <esther191299@gmail.com>
2019-09-15 19:27:48 +0200

    First commit

---
+++ src/basicArrayExercise2/Main.java
@@ -0,0 +1,20 @@
+package basicArrayExercise2;	//Result = Prints all Arrays using toString Method
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String [] fruits = new String [8];
+		fruits [0] = "Apple";
+		fruits [1] = "Banana";
+		fruits [2] = "Pear";
+		fruits [3] = "Mandarine";
+		fruits [4] = "Orange";
+		fruits [5] = "Plumbs";
+		fruits [6] = "Melon";
+		fruits [7] = "Grapes";
+		
+		System.out.println(Arrays.toString(fruits));
+	}
+}
